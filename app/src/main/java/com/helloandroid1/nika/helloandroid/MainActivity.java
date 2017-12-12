9bf88a82e30b4ed3bf0e5d1da544ccf4a30269e4
nikanda <bera.moldavskaya@gmail.com>
2017-12-13 00:48:21 +0200

    working app. Text parameters moved to style

--- app/src/main/java/com/helloandroid1/nika/helloandroid/MainActivity.java
+++ app/src/main/java/com/helloandroid1/nika/helloandroid/MainActivity.java
@@ -1,7 +1,7 @@
 package com.helloandroid1.nika.helloandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
